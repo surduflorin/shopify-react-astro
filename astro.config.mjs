5303c156331f794854369a9d2782c9ec522b78c8
surdu.florin <surdu.florin@hogarthww.digital>
2022-11-22 00:33:11 +0200

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import react from '@astrojs/react';
-integrations: [
-  react(),
-]
+
 export default {
   buildOptions: {
     site: 'https://shopify-astro.netlify.app/',
   },
   renderers: ['@astrojs/renderer-react'],
+  integrations: [
+    react(),
+  ]
 };
