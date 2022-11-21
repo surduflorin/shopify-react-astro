5bb634c242585886a7ab9094cf1e2e98fa320d27
surdu.florin <surdu.florin@hogarthww.digital>
2022-11-22 01:14:28 +0200

    files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   // buildOptions: {
   //   site: 'https://shopify-astro.netlify.app/',
